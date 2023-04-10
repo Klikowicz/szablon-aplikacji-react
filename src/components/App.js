6a1e18f4b67b573fbb0941f071d606170ef7bd27
Janusz Klikowicz <janusz.klikowicz@windowslive.com>
2023-04-10 14:02:53 +0200

    change default app

---
+++ src/components/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+
+import './App.css';
+
+class App extends Component {
+  state = {  } 
+  render() { 
+    return (
+      <React.Fragment>
+        <section>
+          <h1>Szablon aplikacji React</h1>
+        </section>
+      </React.Fragment>
+    );
+  }
+}
+ 
+export default App;
+
+
+
